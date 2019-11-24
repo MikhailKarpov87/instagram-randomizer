47a2345c3dd95d648320489c858457239b142d82
Mikhail Karpov <mihanikk@gmail.com>
2019-11-24 15:07:29 +0300

    Updates

---
+++ precache-manifest.c55f7a2420bb613aea222dfeaed3a6b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5abef3694d3d973ff39ea13a92013eda",
+    "url": "/instagram-randomizer/index.html"
+  },
+  {
+    "revision": "250c3e18451f49fa8400",
+    "url": "/instagram-randomizer/static/css/main.7bbbdfef.chunk.css"
+  },
+  {
+    "revision": "c00357ec670c37d986e7",
+    "url": "/instagram-randomizer/static/js/2.16cb3c75.chunk.js"
+  },
+  {
+    "revision": "250c3e18451f49fa8400",
+    "url": "/instagram-randomizer/static/js/main.ae9d7302.chunk.js"
+  },
+  {
+    "revision": "cb85c8e41f855ebebe89",
+    "url": "/instagram-randomizer/static/js/runtime~main.5487ff15.js"
+  }
+]);
